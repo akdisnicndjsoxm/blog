602384acbe24768d1b73542f7fcb130916a7fbfe
akdisnicndjsoxm <skzkmk@outlook.com>
2024-11-10 15:59:13 +0800

    main

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-btn').addEventListener('click', function() {
-    alert('点击了 登录 按钮');
-});
-
-document.getElementById('register-btn').addEventListener('click', function() {
-    alert('点击了 注册 按钮');
-});
